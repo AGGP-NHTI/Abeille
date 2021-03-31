24ffb60d2d2adff8ba83c271192da28383b197be
erikwilder9009 <erikwilder9009@gmail.com>
2021-03-31 16:06:44 -0400

    Prototype

--- Assets/Scripts/Guy.cs
+++ Assets/Scripts/Guy.cs
@@ -5,29 +5,50 @@ using UnityEngine;
 public class Guy : MonoBehaviour
 {
     Rigidbody2D RB;
+    bool Grounded;
+    public GameObject feet;
+    Legs Legs;
 
     // Start is called before the first frame update
     void Start()
     {
         RB = gameObject.GetComponent<Rigidbody2D>();
+        Grounded = false;
+        Legs = feet.GetComponent<Legs>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKeyDown(KeyCode.W) && Grounded)
         {
             RB.AddForce(new Vector2(0, 150));
+            Grounded = false;
         }
         if(Input.GetKey(KeyCode.D))
         {
             RB.AddForce(new Vector2(1, 0));
+            Legs.Walk(feet, -1);
         }
         if(Input.GetKey(KeyCode.A))
         {
             RB.AddForce(new Vector2(-1, 0));
-            
+            Legs.Walk(feet, 1);
+
+
         }
 
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Grounded = true;
+        Debug.Log("landed");
+    }
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        Grounded = false;
+    }
+
+
 }
---
+++ Assets/Scripts/Legs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Legs : MonoBehaviour
+{
+    public Transform foot1;
+    public Transform foot2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foot1.transform.rotation = Quaternion.Euler(0.0f, 0.0f, gameObject.transform.rotation.z * -1.0f);
+        foot2.transform.rotation = Quaternion.Euler(0.0f, 0.0f, gameObject.transform.rotation.z * -1.0f);
+    }
+
+    public void Walk(GameObject feets,float direction)
+    {
+        feets.transform.Rotate(new Vector3 (0,0,4f * direction));
+    }
+}
