1d3a7e57c98a88148727c80780c0347d0e386373
Kai G <jedipokemon127@yahoo.com>
2021-04-12 12:39:12 -0400

    mainmenu

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+        Debug.Log("Application has been snaped.");
+    }
+}
